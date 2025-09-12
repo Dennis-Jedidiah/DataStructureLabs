db97a8c633a3f51ed5f728fd8ba3b2c96f750037
Ashwin Siva <ashwinsiva@Ashwins-MacBook-Pro.local>
2025-09-12 10:08:01 -0400

    make burrito constructors

--- Lab1/Burrito.java
+++ Lab1/Burrito.java
@@ -9,20 +9,37 @@ public abstract class Burrito {
 
  public Burrito() {
   this.size = "small";
-  this.protein= "none";
-  this.rice= "none";
-  this.beans= "none";
-  this.toppings= "none";
+  this.protein= "no";
+  this.rice= "no";
+  this.beans= "no";
+  this.toppings= "no";
   this.guac= false ;
  } 
 
  public Burrito(String size, String protein){
   this.size= size;
   this.protein= protein;
-   this.rice= "none";
-  this.beans= "none";
-  this.toppings= "none";
+  this.rice= "no";
+  this.beans= "no";
+  this.toppings= "no";
   this.guac= false ;
  }
+ Burrito(String size, String protein, String rice, 
+ String beans, String toppings, boolean guac){
+  this.size= size;
+  this.protein= protein;
+  this.rice= rice;
+  this.beans= beans;
+  this.toppings= toppings;
+  this.guac= guac;
+ }
+ 
+ public String toString(){
+  // size + protein + " Burrito with " + beans + " beans " + toppings + 
+  return this.size + this.protein + " Burrito with " + this.rice 
+  + this.beans + this.toppings + this.guac;
+  
+ }
+
 
 }
