570d485b04bb30d11a50138a0cfa2821869f5781
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2025-09-12 10:24:06 -0400

    minor changes

--- Lab1/Burrito.java
+++ Lab1/Burrito.java
@@ -1,45 +1,45 @@
-public abstract class Burrito {
- 
- String size = "";
- String protein= "";
- String rice = "";
- String beans = "";
- String toppings= "";
- Boolean guac= false;
+public class Burrito {
 
- public Burrito() {
-  this.size = "small";
-  this.protein= "no";
-  this.rice= "no";
-  this.beans= "no";
-  this.toppings= "no";
-  this.guac= false ;
- } 
+    String size;
+    String protein;
+    String rice;
+    String beans;
+    String toppings;
+    Boolean guac;
 
- public Burrito(String size, String protein){
-  this.size= size;
-  this.protein= protein;
-  this.rice= "no";
-  this.beans= "no";
-  this.toppings= "no";
-  this.guac= false ;
- }
- Burrito(String size, String protein, String rice, 
- String beans, String toppings, boolean guac){
-  this.size= size;
-  this.protein= protein;
-  this.rice= rice;
-  this.beans= beans;
-  this.toppings= toppings;
-  this.guac= guac;
- }
- 
- public String toString(){
-  // size + protein + " Burrito with " + beans + " beans " + toppings + 
-  return this.size + this.protein + " Burrito with " + this.rice 
-  + this.beans + this.toppings + this.guac;
-  
- }
+    public Burrito() {
+        size = "small";
+        protein = "no";
+        rice = "no";
+        beans = "no";
+        toppings = "no";
+        guac = false;
+    }
 
+    public Burrito(String size, String protein) {
+        this.size = size;
+        this.protein = protein;
+        this.rice = "no";
+        this.beans = "no";
+        this.toppings = "no";
+        this.guac = false;
+    }
+
+    Burrito(String size, String protein, String rice,
+            String beans, String toppings, boolean guac) {
+        this.size = size;
+        this.protein = protein;
+        this.rice = rice;
+        this.beans = beans;
+        this.toppings = toppings;
+        this.guac = guac;
+    }
+
+    public String toString() {
+        // size + protein + " Burrito with " + beans + " beans " + toppings +
+        return this.size + this.protein + " Burrito with " + this.rice
+                + this.beans + this.toppings + this.guac;
+
+    }
 
 }
