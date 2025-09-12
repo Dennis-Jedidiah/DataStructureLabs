ea15a2dfb279fa6da5de8d6f00c10e47a2782396
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2025-09-12 10:56:37 -0400

    part 2

--- Lab1/Burrito.java
+++ Lab1/Burrito.java
@@ -33,55 +33,80 @@ public class Burrito {
         this.guac = guac;
     }
 
+    // Getters
     public String getRice() {
         if (this.rice == "none") {
-            return("no rice");
+            return("no rice ");
         } else {
-            return(this.rice);
-            
+            return(this.rice + " ");
         }
     }
 
     public String getSize() {
         if (this.size == "none") {
-            return("small");
+            return("small ");
         } else {
-            return(this.size);
+            return(this.size + " ");
         }
     }
     
     public String getProtein() {
         if (this.protein == "none") {
-            return("no protein");
+            return("no protein ");
         } else {
-            return(this.protein);
+            return(this.protein + " ");
         }
     }
 
     public String getBeans() {
         if (this.beans == "none") {
-            return("no beans");
+            return("no beans ");
         } else {
-            return(this.beans);
+            return(this.beans + " ");
         }
     }
 
     public String getToppings() {
         if (this.toppings == "none") {
-            return("no toppings");
+            return("no toppings ");
         } else {
-            return(this.toppings);
+            return(this.toppings + " ");
         }
     }
 
     public String getGuac() {
         if (this.guac == false) {
-            return("no guacamole");
+            return("no guacamole ");
         } else {
-            return("guacamole");
+            return("guacamole ");
         }
     }
+    public double getCost() {
+        double cost = 5.00;
+        if (this.size == "large") {
+            cost += 2.50;
+        }
+        if (this.protein == "chicken" || this.protein == "steak") {
+            cost += 2.00;
+        } else {
+            cost += 3.00;
+        }
+        if (this.rice != "none") {
+            cost += 1.00;
+        }
+        if (this.beans != "none") {
+            cost += 1.00;
+        }
+        if (this.toppings != "none") {
+            cost += 0.00;
+        }
+        if (this.guac == true) {
+            cost += 2.00;
+        }
+        return cost;
+    }
 
+    // Setters
     public void setRice(String rice) {
         this.rice = rice;
     }
@@ -107,6 +132,6 @@ public class Burrito {
     }
     public String toString(){
         return size + protein + " burrito with " + rice + ", " + beans + ", "
-                + toppings + ", and" + guac + ".";
+                + toppings + ", and" + guac + ". \n Total cost: $" + getCost();
     }
 }
--- Lab1/BurritoOrder.java
+++ Lab1/BurritoOrder.java
@@ -1,24 +1,30 @@
 public class BurritoOrder {
 
- public static void main(String[] args) {
-  Burrito newBurrito = new Burrito();
-  newBurrito.setSize("small");
-  newBurrito.setProtein("chicken");
-  newBurrito.setRice("white");
-  newBurrito.setBeans("brown");
-  newBurrito.setToppings("lettuce");
-  newBurrito.setGuac(true);
+    public static void main(String[] args) {
+        Burrito newBurrito = new Burrito();
+        newBurrito.setSize("small");
+        newBurrito.setProtein("chicken");
+        newBurrito.setRice("white");
+        newBurrito.setBeans("brown");
+        newBurrito.setToppings("lettuce");
+        newBurrito.setGuac(true);
 
-  Burrito SecondBurrito = new Burrito("Large", "Steak");
+        Burrito SecondBurrito = new Burrito("Large", "Steak");
 
-  SecondBurrito.setRice("white");
-  SecondBurrito.setBeans("Brown");
-  SecondBurrito.setToppings("lettuce, Chesse, Olives");
-  SecondBurrito.setGuac(true);
+        SecondBurrito.setRice("white");
+        SecondBurrito.setBeans("Brown");
+        SecondBurrito.setToppings("lettuce, Chesse, Olives");
+        SecondBurrito.setGuac(true);
 
-  Burrito ThirdBurrito = new Burrito("Large", "Steak", "Brown", "Black", "Tomatoes", false);
-  System.out.println("Burrito 1: " + newBurrito);
-  System.out.println("Burrito 2: " + SecondBurrito);
-  System.out.println("Burrito 3: " + ThirdBurrito);
- }
+        Burrito ThirdBurrito = new Burrito("Large", "Steak", "Brown", "Black", "Tomatoes", false);
+        Burrito FourthBurrito = new Burrito("Small", "Tofu", "None", "None", "olives", false);
+        Burrito FifthBurrito = new Burrito("large", "carnita", "None", "None", "No Toppings", true);
+        Burrito SixthBurrito = new Burrito("Small", "chicken", "None", "None", "Lettuce, cheese", false);
+        System.out.println("Burrito 1: " + newBurrito);
+        System.out.println("Burrito 2: " + SecondBurrito);
+        System.out.println("Burrito 3: " + ThirdBurrito);
+        System.out.println("Burrito 4: " + FourthBurrito);
+        System.out.println("Burrito 5: " + FifthBurrito);
+        System.out.println("Burrito 6: " + SixthBurrito);
+    }
 }
