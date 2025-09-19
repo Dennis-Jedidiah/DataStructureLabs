0365cdb96bb0ff330fe28cffae400747bda696ea
Dennis Jedidiah <thejedidiahdennis@gmail.com>
2025-09-19 10:12:40 -0400

    Edit Node.java to work with generic file types

--- Lab2/Node.java
+++ Lab2/Node.java
@@ -1,38 +1,38 @@
-public class Node {
-    private int data;
-    private Node prev;
-    private Node next;
+public class Node<E> {
+    private E data;
+    private Node<E> prev;
+    private Node<E> next;
 
     // Constructor
-    public Node(int data) {
+    public Node(E data) {
         this.data = data;
         this.prev = null;
         this.next = null;
     }
 
     // Getters
-    public int getData() {
+    public E getData() {
         return data;
     }
 
-    public Node getPrev() {
+    public Node<E> getPrev() {
         return prev;
     }
 
-    public Node getNext() {
+    public Node<E> getNext() {
         return next;
     }
 
     // Setters
-    public void setData(int data) {
+    public void setData(E data) {
         this.data = data;
     }
 
-    public void setPrev(Node prev) {
+    public void setPrev(Node<E> prev) {
         this.prev = prev;
     }
 
-    public void setNext(Node next) {
+    public void setNext(Node<E> next) {
         this.next = next;
     }
 }
